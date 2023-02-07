9016cae7b2865d1db93299cd8d19caf78f284a11
sham-10 <sham.kk.mis@gmail.com>
2023-02-07 13:14:01 +0800

    2nd push update

--- src/test/java/training/FirstSeleniumJavaTest.java
+++ src/test/java/training/FirstSeleniumJavaTest.java
@@ -1,53 +1,51 @@
 package training;
 
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
-import org.openqa.selenium.By;		
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;		
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;		
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstSeleniumJavaTest {
+	
+	WebDriver driver;
+	@Test
+	public void TestChromeDriver() {
+		
+		
 
-	private WebDriver driver;		
-	@Test				
-	public void testEasy() {
-
+		
 		driver.get("https://testautomationu.applitools.com/"); 
-		driver.manage().window().maximize();
-
+	 	driver.manage().window().maximize();		
 		String title = driver.getTitle();
+		System.out.println("TITLE: "+title);
+		DateTimeFormatter formatD =  DateTimeFormatter.ofPattern("yyy MMdd HH:MM:SS");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		AssertJUnit.assertTrue(title.equals("Test Automation University | Applitools"));
-	}	
 
-	@Test				
-	public void testTwo() {	
 
-		driver.get("https://www.google.co.uk/"); 
-		driver.manage().window().maximize();
-
-		String title1 = driver.getTitle();
-		System.out.println("title is "+title1);
-		AssertJUnit.assertTrue(title1.equals("Google"));
-
-	}	
-
+	}
+	
 	@BeforeTest
 	public void beforeTest() {	
-	    driver = new ChromeDriver();  
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(chromeOptions);
 	}		
 	@AfterTest
 	public void afterTest() {
 		driver.quit();			
 	}	
+
 }
